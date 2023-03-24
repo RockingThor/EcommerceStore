2f5f8b403e3b20171ab398ad8917144aee14dd8d
Rohit Nandi <rohitnandibmp@gmail.com>
2023-03-24 23:57:55 +0530

    Commit after end of 24 mar

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,47 @@
-const express= require('express');
+const express = require('express');
 const app = express();
-
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv/config');
 
-const api= process.env.API_URL;
+app.use(cors());
+app.options('*', cors())
 
-//app.use(express.json); 
-const morgan= require('morgan');
+//middleware
+app.use(express.json());
 app.use(morgan('tiny'));
 
-app.get(`${api}/products`,(req, res)=>{
-    const product={
-        id: 1,
-        name: "Laptop",
-        image: "/"
-    }
-    res.send(product);
+
+//Routes
+const categoriesRoutes = require('./routes/categories');
+const productsRoutes = require('./routes/products');
+const usersRoutes = require('./routes/users');
+const ordersRoutes = require('./routes/orders');
+
+const api = process.env.API_URL;
+
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/products`, productsRoutes);
+app.use(`${api}/users`, usersRoutes);
+app.use(`${api}/orders`, ordersRoutes);
+
+//Database
+mongoose.connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'e-shop-database'
+})
+.then(()=>{
+    console.log('Database Connection is ready...')
+})
+.catch((err)=> {
+    console.log(err);
 })
 
+//Server
 app.listen(3000, ()=>{
-    console.log("The surver is running now on http://localhost:3000");
+
+    console.log('server is running http://localhost:3000');
 })
